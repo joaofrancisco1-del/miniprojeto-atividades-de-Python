69cb5a9a3c371172549ba8511539616e5b122959
joaofrancisco1-del <joao.francisco.1@academico.ifpb.edu.br>
2026-06-11 20:57:10 -0300

    bug de repetição e função de vencedor adicionado

--- oficial.py
+++ oficial.py
@@ -1,13 +1,13 @@
-def jogada1(a,b,matriz,f,matriz2):
+def jogada1(a,b,matriz,f):
     jogue=[]
     for p in b:
         jogue.append(p)
     for r in jogue:
-        if r == 'a':
+        if r == 'a' or r == "A":
             jogue[1]= 0
-        if r == 'b':
+        if r == 'b' or r == "B":
             jogue[1]= 1
-        if  r == 'c':
+        if  r == 'c' or r == "C":
             jogue[1]= 2
         if r == '1':
             jogue[0]= 0
@@ -17,13 +17,10 @@ def jogada1(a,b,matriz,f,matriz2):
             jogue[0]= 2
     i = jogue[0]
     j = jogue[1]
-    if matriz[i][j] != " ":
+    if jogue [1] not in [0,1,2]:
         f += 1
     elif matriz[i][j] == " ":
         matriz[i][j] = a
-    if matriz2 [i][j] == "":
-        matriz2 [i][j]=a
-    
 def resultado(matriz):#jogo da velha
     
     print("   A   B   C")
@@ -44,29 +41,23 @@ def resultado(matriz):#jogo da velha
         if i != 2:
             print("  ---+---+---")
 def teste(matriz,b):
-    x = 0
-    if matriz[0][0] == matriz[0][1] and matriz[0][0] == matriz[0][2] and matriz[0][1] == matriz[0][2]:
-        x += 1
-    if matriz[0][0] == matriz[1][0] and matriz[0][0] == matriz[2][0] and matriz[1][0] == matriz[2][0]:
-        x += 1  
-    if matriz[0][1] == matriz[1][1] and matriz[0][0] == matriz[2][1] and matriz[1][1] == matriz[2][1]:
-        x += 1
-    
-    if matriz[0][2] == matriz[1][2] and matriz[0][2] == matriz[2][2] and matriz[1][2] == matriz[2][2]:
-        x += 1
-    if matriz[1][0] == matriz[1][1] and matriz[1][0] == matriz[1][2] and matriz[1][1] == matriz[1][2]:
-        x += 1
-        
-    if matriz[2][0] == matriz[2][1] and matriz[2][0] == matriz[2][2] and matriz[2][1] == matriz[2][2]:
-        x += 1
-    
-    if matriz[0][0] == matriz[1][1] and matriz[0][0] == matriz[2][2] and matriz[1][1] == matriz[2][2]:
-        x += 1
-        
-    if matriz[0][2] == matriz[1][1] and matriz[0][2] == matriz[2][0] and matriz[1][1] == matriz[2][0]:
-        x += 1
-    if x != 0:
+    if matriz[0][0] == matriz[0][1] and matriz[0][0] == matriz[0][2] and matriz[0][1] == matriz[0][2] and matriz[0][0] != " " and matriz[0][1] != " " and matriz[0][2] != " ":
         b += 1
+    elif matriz[0][0] == matriz[1][0] and matriz[0][0] == matriz[2][0] and matriz[1][0] == matriz[2][0] and matriz[0][0] != " " and matriz[1][0] != " " and matriz[2][0] != " ":
+        b += 1  
+    elif matriz[0][1] == matriz[1][1] and matriz[0][0] == matriz[2][1] and matriz[1][1] == matriz[2][1] and matriz[0][1] != " " and matriz[1][1] != " " and matriz[2][1] != " ":
+        b += 1
+    elif matriz[0][2] == matriz[1][2] and matriz[0][2] == matriz[2][2] and matriz[1][2] == matriz[2][2] and matriz[0][2] != " " and matriz[1][2] != " " and matriz[2][2] != " ":
+        b += 1
+    elif matriz[1][0] == matriz[1][1] and matriz[1][0] == matriz[1][2] and matriz[1][1] == matriz[1][2] and matriz[1][0] != " " and matriz[1][1] != " " and matriz[1][2] != " ":
+        b += 1
+    elif matriz[2][0] == matriz[2][1] and matriz[2][0] == matriz[2][2] and matriz[2][1] == matriz[2][2] and matriz[2][0] != " " and matriz[2][1] != " " and matriz[2][2] != " ":
+        b += 1
+    elif matriz[0][0] == matriz[1][1] and matriz[0][0] == matriz[2][2] and matriz[1][1] == matriz[2][2] and matriz[0][0] != " " and matriz[1][1] != " " and matriz[2][2] != " ":
+        b += 1
+    elif matriz[0][2] == matriz[1][1] and matriz[0][2] == matriz[2][0] and matriz[1][1] == matriz[2][0] and matriz[0][2] != " " and matriz[1][1] != " " and matriz[2][0] != " ":
+        b += 1
+    return b
 def placar(a,b):
     if a == 3 and b < 3:
         print("JOGADOR 1 VENCEU!")
@@ -81,57 +72,51 @@ print("JOGO DA VELHA")#começo do jogo
 sair = False
 ctr = 0
 matriz = [[" "," "," "],[" "," "," "],[" "," "," "]]
-matriz2 = [[" "," "," "],[" "," "," "],[" "," "," "]]#criei uma matriz clone que vou aplicar os numeros, pra fazer a soma e ver se ganhou ou nao
+#faltando corrigir as jogadas onde a posição ja esta ocupada!!!
 while True:
     resultado(matriz)
+    print()
     a1 = str(input("DIGITE SEU CARACTER JOGADOR 1: "))
     a2 = str(input("DIGITE SEU CARACTER JOGADOR 2: "))
     pont1 = 0
     pont2 = 0
     ctr=1
     z = 0
-    for x in range(9):
+    for x in range(1,10):
         if ctr%2!=0:
+            c = 0
             while True:
                 d = 0
                 pos1 = str(input("DIGITE SUA JOGADA JOGADOR 1: "))
+                print()
                 jogada1(a1,pos1,matriz,d)
                 if d == 0:
                     break
                 else:
                     print("DIGITE UMA JOGADA VÁLIDA")
-        resultado(matriz)
-        teste(matriz,z)
-        if ctr%2==0:
+                    print()
+            resultado(matriz)
+            print()
+        if teste(matriz,c) != 0:
+            pont1+=1
+            break
+        elif ctr%2==0:
+            c = 0
             while True:
-                pos2=str(input("digita sua jogada jogador 2"))
+                d = 0
+                pos2 = str(input("DIGITE SUA JOGADA JOGADOR 2: "))
+                print()
                 jogada1(a2,pos2,matriz,d)
                 if d == 0:
                     break
                 else:
                     print("DIGITE UMA JOGADA VÁLIDA")
-        resultado(matriz)
-        teste(matriz,z)
-        if z == 1:
-            pont1+=1
-            break
-            
-        elif ctr%2==0:
-            if ctr%2!=0:
-                while True:
-                    d = 0
-                    pos1 = str(input("DIGITE SUA JOGADA JOGADOR 2: "))
-                    jogada1(a2,pos1,matriz,d)
-                    if d != 0:
-                        break
-                    else:
-                        print("DIGITE UMA JOGADA VÁLIDA")
-            jogada1(a2,pos1,matriz,d)
+                    print()
             resultado(matriz)
-            teste(matriz,z)
-            if z == 1:
-                pont1+=1
-                break
+            print()
+        if teste(matriz,c) != 0:
+            pont2+=1
+            break
         ctr+=1
     if z == 0:
         print("EMPATE!")
